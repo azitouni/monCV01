4c687d69d94835dbc5929be1785b2956770ab50c
azitouni <zitouniadel@hotmail.com>
2016-12-08 16:57:38 +0100

    màj js file

--- js/script.js
+++ js/script.js
@@ -1,12 +1,55 @@
 $(function() {
-  var $css = $("#css").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.6, onProgressUpdate : function(val) {
+  //CSS
+  var $css = $("#css").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
     $css.setValue(Math.round(val * 100.0));
   }});
   $css.setValue('CSS');
-  var $js = $("#js").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.35, onProgressUpdate : function(val) {
+  //JS
+  var $js = $("#js").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
     $js.setValue(Math.round(val * 100.0));
   }});
   $js.setValue('Javascript');
+  //PHP
+  var $php = $("#php").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
+    $php.setValue(Math.round(val * 100.0));
+  }});
+  $php.setValue('PHP');
+  //Bootstrap
+  var $bt = $("#bootstrap").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
+    $bt.setValue(Math.round(val * 100.0));
+  }});
+  $bt.setValue('Bootstrap');
+  //JQuery
+  var $jq = $("#JQuery").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
+    $jq.setValue(Math.round(val * 100.0));
+  }});
+  $jq.setValue('JQUERY');
+  //ng
+  var $ng = $("#ng").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
+    $ng.setValue(Math.round(val * 100.0));
+  }});
+  $ng.setValue('Angular');
+  //MySql
+  var $ms = $("#MySQL").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
+    $ms.setValue(Math.round(val * 100.0));
+  }});
+  $ms.setValue('MySQL');
+  //SqlServer
+  var $ss = $("#SqlServer").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
+    $ss.setValue(Math.round(val * 100.0));
+  }});
+  $ss.setValue('SqlServer');
+  //CSHARP
+  var $ss = $("#CSharp").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
+    $ss.setValue(Math.round(val * 100.0));
+  }});
+  $ss.setValue('CSharp');
+  //ASPNET
+  var $ss = $("#AspNet").percentageLoader({width: 256, height: 256, controllable : true, progress : 0.5, onProgressUpdate : function(val) {
+    $ss.setValue(Math.round(val * 100.0));
+  }});
+  $ss.setValue('ASP NET');
+
 
   // var topLoaderRunning = false;
   // $("#cssBtn").click(function() {
